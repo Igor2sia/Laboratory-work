0a6b9214a6fe135a01bceaa95d0bf722650f08eb
4pok <91655233+Igor2sia@users.noreply.github.com>
2024-04-08 00:26:16 +0300

    Delete oop/oop directory

--- oop/oop/AppleTreeMain.java
+++
@@ -1,7 +0,0 @@
-package oop;
-
-public class AppleTreeMain {
-    public static void main(String[] args) {
-
-    }
-}
--- oop/oop/Main.java
+++
@@ -1,8 +0,0 @@
-package oop;
-
-public class Main {
-    public static void main(String[] args) {
-        Car car = new Car("Chevrolet","Impala", 390);
-        System.out.println(car.getInfo());
-    }
-}
