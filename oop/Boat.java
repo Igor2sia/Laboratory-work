7eddba5476b0dccd08e1a1a7633ea0cb2a53e541
4pok <91655233+Igor2sia@users.noreply.github.com>
2024-05-03 11:09:19 +0300

    Add files via upload

--- oop/Boat.java
+++ oop/Boat.java
@@ -1,11 +1,22 @@
-import java.util.Scanner;
+public class Boat extends Vehicle {
 
-public class BoatMain {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int pas = scanner.nextInt();
-        int oars = scanner.nextInt();
-        Boat boat = new Boat(pas, oars);
-        System.out.println(boat.toString());
+    private int oarsCount;
+
+    public Boat(int passengers, int oarsCount) {
+        super(passengers);
+        setPassengers(passengers);
+        setOarsCount(oarsCount);
+    }
+
+    private void setOarsCount(int oarsCount) {
+        if (oarsCount < 0) throw new IllegalArgumentException("Invalid negative number! Argument: oars count");
+        if (oarsCount == 0) throw new IllegalArgumentException("Need oars to swim!");
+        if (oarsCount > passengers * 2) throw new IllegalArgumentException("One too many oars!");
+        this.oarsCount = oarsCount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("In a Boat %d passengers with %d oars",passengers,oarsCount);
     }
 }
--- oop/Main.java
+++ oop/Main.java
@@ -1,8 +1,11 @@
-package oop;
+import java.util.Random;
 
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Chevrolet","Impala", 390);
-        System.out.println(car.getInfo());
+        Random random = new Random();
+        System.out.println(random.nextInt(1,6));
+        System.out.println(random.nextInt(1,6));
     }
 }
