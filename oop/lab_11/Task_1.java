d526e83f0a2268e93bea820d9701aefd7f0410b9
4pok <91655233+Igor2sia@users.noreply.github.com>
2024-05-27 05:06:51 +0300

    Add files via upload

---
+++ oop/lab_11/Task_1.java
@@ -0,0 +1,29 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Task_1 {
+    static String path = "resources\\example.txt";
+    static Scanner scanner = new Scanner(System.in);
+    static String text = "";
+
+    public static void main(String[] args){
+        String inputLast = "";
+        while (!text.equalsIgnoreCase("end")){
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("end"))
+                write(inputLast);
+
+            inputLast = input;
+        }
+    }
+    static void write (String text){
+        try (FileWriter writer = new FileWriter(path, true)) {
+            writer.write(text);
+            writer.write(System.lineSeparator());
+            writer.flush();
+        }catch (IOException e){
+            System.out.println("Exeption" + e.getMessage());
+        }
+    }
+}
